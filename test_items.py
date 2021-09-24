89e18f418945ac15256596f2c56fda3faaf78d52
Проничев Владислав Викторович <v.pronichev@r58ci.local>
2021-09-24 14:40:50 +0300

    add function for find cart button on type

--- test_items.py
+++ test_items.py
@@ -1,1 +1,10 @@
+import time
 
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_check_cart_button(browser):
+	browser.get(link)
+	time.sleep(5)
+	button = browser.find_element_by_css_selector("[type='submit']").is_displayed()
+	assert button, "There is not button!"
