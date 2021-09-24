4a0263921bd6b5b3ac6e7df0e6c30e76bdfbba39
Проничев Владислав Викторович <v.pronichev@r58ci.local>
2021-09-24 14:35:43 +0300

    add browser fixture for chooice language and work in Chrome browser

--- conftest.py
+++ conftest.py
@@ -1,1 +1,18 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default=None,
+                     help="Choose language")
+
+	
+@pytest.fixture(scope="function")
+def browser(request):
+	options = Options()
+	user_language = request.config.getoption("language")
+	options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+	browser = webdriver.Chrome(options=options)
+	yield browser
+	browser.quit()
